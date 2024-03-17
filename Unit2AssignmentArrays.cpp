587107421d76b0a95c5fbee9dbaf658c7f0c1bc1
timerr <timothy.errickson@gmail.com>
2024-03-16 22:56:45 -0500

    Array Finalized
    Arrays assignment finalized

--- Unit2AssignmentArrays.cpp
+++ Unit2AssignmentArrays.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
 using namespace std;
 
+//constants for number of Rows and Cols
 const int ROWS = 3;
 const int COLS = 3;
 
-// Function to insert data into the array
+// inserts data into the array
 void insertData(int arr[][COLS]) {
     cout << "Enter " << ROWS * COLS << " elements for the array:" << endl;
     for (int i = 0; i < ROWS; ++i) {
         for (int j = 0; j < COLS; ++j) {
-            // Prompting the user to enter each element of the array
+            // Prompt the user to enter each element of the array
             cout << "Enter element at position [" << i << "][" << j << "]: ";
-            cin >> arr[i][j]; // Reading user input and storing it in the array
+            cin >> arr[i][j]; // Read user input and store it in the array
         }
     }
 }
 
-// Function to print the contents of the array
+// print the contents of the array
 void printArray(int arr[][COLS]) {
     cout << "Array contents:" << endl;
     for (int i = 0; i < ROWS; ++i) {
         for (int j = 0; j < COLS; ++j) {
-            // Printing each element of the array
+            // Print each element of the array
             cout << arr[i][j] << " ";
         }
         cout << endl; // Moving to the next line after printing each row
@@ -31,8 +32,8 @@ void printArray(int arr[][COLS]) {
 int main() {
     int array[ROWS][COLS]; // Declaring a 3x3 array
 
-    insertData(array); // Calling insertData function to populate the array
-    printArray(array); // Calling printArray function to display the array
+    insertData(array); // Call insertData function to populate the array
+    printArray(array); // Call printArray function to display the array
 
     return 0;
 }
